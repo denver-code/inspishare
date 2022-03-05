68f68cf6355b8949d8d42bf0b6c3732a0bfa049b
Denver <csigorek@gmail.com>
2022-03-05 19:10:04 +0200

    created main dude screen

---
+++ lib/screens/mainscreen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          SizedBox(
+            height: 45,
+          ),
+          Text("SOME TEXT NIGGERS!!!!!!!")
+        ],
+      ),
+    );
+  }
+}
